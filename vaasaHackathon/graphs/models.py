592c5b64642633279384bf9a14554d433f98d01e
Anouar El Haddadi El Yahyati <anouar.el.haddadi@estudiantat.upc.edu>
2025-09-27 22:53:50 +0300

    connect to postgres

--- vaasaHackathon/graphs/models.py
+++ vaasaHackathon/graphs/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
 # Create your models here.
-
+class Proces(models.Model):
+    lci_number = models.IntegerField(primary_key=True)
+    unit = models.TextField()
+    process_name = models.TextField()
+    carbon_dioxide = models.IntegerField()
+    total_eco_costs = models.IntegerField()
+    eco_costs_e1 = models.IntegerField()
+    eco_costs_e2 = models.IntegerField()
+    eco_costs_e3 = models.IntegerField()
+    eco_costs_e4 = models.IntegerField()
+    eco_costs_e5 = models.IntegerField()
  
