d03cee26088c200ba55ac12884c764303d461248
XinshenXiangChen <xinshenxiangchen@gmail.com>
2025-09-28 02:10:27 +0200

    loads better

--- vaasaHackathon/graphs/views.py
+++ vaasaHackathon/graphs/views.py
@@ -1,5 +1,9 @@
 ﻿from django.shortcuts import render
 import grafana_api
+import pandas as pd
+
+from .llm import load_items_from_csv, map_items_to_idemat
+
 
 # Create your views here.
 
@@ -7,14 +11,18 @@ def index(request):
     return render(request, 'index.html')
 
 def upload_csv(request):
+    context = []
     if request.method == 'POST' and request.FILES.get('csv_file'):
         uploaded_file = request.FILES["csv_file"]
-
+        csv_items = load_items_from_csv(uploaded_file)
+        result = map_items_to_idemat(csv_items["Issue"].tolist(), topn=20)
         # Read file contents
-        file_data = uploaded_file.read().decode("utf-8")  # assuming text file
-        print(file_data)  # prints to Django console
-        # Handle CSV upload here
-        pass
+
+        result_listed = [progress['matched_process'] for progress in result]
+        df_result = pd.DataFrame({"Issue": csv_items["Issue"],
+                                  "Categories": result_listed,
+                                  "Quantity": csv_items["Quantity"]})
+        print(df_result)
     return render(request, 'index.html')
 
 def api_upload_csv(request):
